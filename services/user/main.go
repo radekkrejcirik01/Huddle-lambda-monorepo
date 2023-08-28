ea0e64b78046d05793d7ad41e5b93638ec26b9d0
radekkrejcirik <radekkrejcirik@Radeks-MacBook-Air.local>
2023-08-28 18:28:23 +0200

    refactor(user): remove unused tables

--- services/user/main.go
+++ services/user/main.go
@@ -4,12 +4,11 @@ import (
 	"context"
 	"log"
 
-	"github.com/radekkrejcirik01/PingMe-backend/services/user/pkg/model/devices"
-
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	fiberadapter "github.com/awslabs/aws-lambda-go-api-proxy/fiber"
 	"github.com/radekkrejcirik01/PingMe-backend/services/user/pkg/database"
+	"github.com/radekkrejcirik01/PingMe-backend/services/user/pkg/model/devices"
 	"github.com/radekkrejcirik01/PingMe-backend/services/user/pkg/model/huddles"
 	"github.com/radekkrejcirik01/PingMe-backend/services/user/pkg/model/messaging"
 	"github.com/radekkrejcirik01/PingMe-backend/services/user/pkg/model/people"
@@ -22,22 +21,22 @@ var fiberLambda *fiberadapter.FiberLambda
 func init() {
 	database.Connect()
 	if err := database.DB.AutoMigrate(
-		&people.Invite{},
-		&people.MutedConversation{},
 		&people.Blocked{},
-		&huddles.HuddleLike{},
+		&messaging.Conversation{},
+		&messaging.ConversationLike{},
+		&devices.Device{},
 		&huddles.Huddle{},
 		&huddles.HuddleComment{},
 		&huddles.HuddleCommentLike{},
+		&huddles.HuddleLike{},
+		&people.Invite{},
 		&huddles.LastSeenHuddle{},
-		&messaging.Conversation{},
-		&messaging.Message{},
-		&messaging.PersonInConversation{},
 		&messaging.LastSeenMessage{},
-		&messaging.ConversationLike{},
+		&messaging.Message{},
 		&messaging.MessageReaction{},
+		&people.MutedConversation{},
+		&messaging.PersonInConversation{},
 		&users.User{},
-		&devices.Device{},
 	); err != nil {
 		log.Fatal(err)
 	}
